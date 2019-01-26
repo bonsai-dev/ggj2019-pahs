5c9891de0b955b91c3d9909a161797dbdd48bc73
Matthias Lücke <ml@backslash-n.com>
2019-01-26 22:40:11 +0100

    Added ClickNPC

--- scripts/ClickNPC.gd
+++ scripts/ClickNPC.gd
@@ -4,27 +4,28 @@ export (int) var speed = 100
 export (float) var accel = 0.1
 
 var motion = Vector2()
-var motion = Vector2()
+
+
+func _ready():
+	
+	$DebugLine.add_point(Vector2())
+	$DebugLine.add_point(Vector2())
+
 
 func _process(delta):
+	
 	update_motion(delta)
 	move_and_slide(motion)
 	
-	$Line2D.set_point_position(0, to_local(position))
-	$Line2D.set_point_position(1, to_local(get_global_mouse_position()))
+	$DebugLine.set_point_position(0, to_local(position))
+	$DebugLine.set_point_position(1, to_local(get_global_mouse_position()))
 
-func _ready():
-	$Line2D.add_point(Vector2())
-	$Line2D.add_point(Vector2())
 
 func update_motion(delta):
-	
-	
 	
 	var newPosition = get_global_mouse_position()
 	var oldPosition = position
 	
-
 	motion = newPosition - oldPosition 
 	
 	if motion.length() > $Sprite.texture.get_width():
@@ -34,3 +35,9 @@ func update_motion(delta):
 		motion = motion * 0
 
 	#debugLabel.text = "Length: " + str(motion.length()) + "\nX: " + str(motion.x) + "\nY: " + str(motion.y)
+
+
+
+func _on_ClickNPC_input_event(viewport, event, shape_idx):
+	print(event)
+	pass # replace with function body
